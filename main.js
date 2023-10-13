7e20614944858b1fb5df508bcda3940e48f4f0b5
Evoldt123 <dannyt2469@gmail.com>
2023-10-13 19:18:52 -0400

    Random & Latex

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
+// Utilities
+function rndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function renderLatex(equation, targetId) {
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, targetId]); // Typeset the target element
+    document.getElementById(targetId).textContent = equation; // Set the content of the target element
+}
+
+// Load Funcitonality
+
+var yoyo = "TESTING";
+
+var question_one_A = rndInteger(1, 10);
+var question_one_B = rndInteger(1, 10);
+var question_one_ans = question_one_A + question_one_B;
+// Events
+
+function onPageLoad() {
+    var equation_q1_a = `\\(${question_one_A} + ${question_one_B}\\)`;
+    document.getElementById("question_one").innerHTML = `What is ${equation_q1_a} ?`
+}
+
 function iHavePolio(event) {
     event.preventDefault();
 
-    var text = 69;
-    var polio = 0;
     var num1 = parseInt(document.getElementById("first-text").value);
-    var num2 = parseInt(document.getElementById("second-text").value);
-    polio = num1 + num2;
-    document.getElementById("thing").innerHTML = polio;
+
+    if (num1 === question_one_ans) {
+        document.getElementById("thing").innerHTML = "Correct";
+    }
+    else {
+        document.getElementById("thing").innerHTML = "Incorrect";
+    }
     
+        
 }
+
