8402e927614daae30f68b519d413cf9eb6acfd23
Evoldt123 <dannyt2469@gmail.com>
2023-10-16 14:39:05 -0400

    Push Q1

--- physics/physics.js
+++ physics/physics.js
@@ -4,8 +4,8 @@ function rndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function rndFloat(min, max, multiple, sigfigs) {
-    let num = (Math.random() * (max - min + 1) + min) * (multiple*10);
+function rndFloat(min, max, sigfigs) {
+    let num = (Math.random() * (max - min + 1) + min);
     return num.toPrecision(sigfigs);
 }
 
@@ -15,21 +15,40 @@ var question_one_ans = question_one_A + question_one_B;
 
 var u1_q2_text = "To be or not to be, that is the question. \\( 69? \\)";
 var u1_question_two_ans = 5;
+// weio
+var u2_q1_mass = rndFloat(10, 100, 2);
+var u2_q1_force = rndFloat(100, 150, 3);
+var u2_q1_theta = rndInteger(10, 50);
+var u2_q1_displacement = rndFloat(10, 30, 3);
 
-var u2_q1_text = rndFloat(1, 100, 3, 5);
-var u2_question_one_ans = 20390;
-
+var u2_q1_text = `Kaden Ingrao is mowin his lawn using his \\( ${u2_q1_mass} \\, \\mathrm{kg} \\) 
+mower using an applied force of \\( ${u2_q1_force} \\, \\mathrm{N} \\) directed \\( ${u2_q1_theta}^\\circ \\)
+below the horizontal. What is the work Mr. Ingrao puts in if he goes \\( ${u2_q1_displacement} \\, \\mathrm{m} \\)?`;
+var u2_question_one_ans_big = Math.cos(u2_q1_theta * Math.PI / 180)*u2_q1_force*u2_q1_displacement;
+var u2_question_one_ans = parseFloat(u2_question_one_ans_big.toPrecision(2));
 // Events
 
 function onPageLoad() {
     var equation_q1_a = `\\(${question_one_A} \\, \\mathrm{m/s} +${question_one_B} \\, \\mathrm{m/s} \\)`;
+
     document.getElementById("question_one").innerHTML = `What is ${equation_q1_a} ?`;
-    document.getElementById("spoiler1").innerHTML = `${equation_q1_a} is \\(${question_one_A+question_one_B} \\, \\mathrm{m/s} \\)`;
+    document.getElementById("spoiler1").innerHTML = `
+    ${equation_q1_a} is \\(${question_one_A+question_one_B} \\, \\mathrm{m/s} \\)`;
 
     document.getElementById("u1_question_two").innerHTML = u1_q2_text;
 
     document.getElementById("u2_question_one").innerHTML = u2_q1_text;
-
+    document.getElementById("u2_spoiler1").innerHTML = `The answer is \\( ${u2_question_one_ans_big} \\, \\mathrm{J} \\) 
+    or \\( ${u2_question_one_ans} \\, \\mathrm{J} \\) 
+    \\( \\\\ \\\\ \\) 
+    Due to the dot product rule: 
+    \\( \\\\ \\) 
+    \\( W = |F| \\, |\\Delta d| \\, \\cos(\\theta) \\) 
+    \\( \\\\ \\)
+    \\( W = ${u2_q1_force} \\, \\mathrm{N} *${u2_q1_displacement} \\, \\mathrm{m} \\, *\\cos(${u2_q1_theta}^\\circ) \\)
+    \\( \\\\ \\)
+    \\( W = ${u2_question_one_ans_big}\\)`;
+    
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
 
@@ -44,8 +63,6 @@ function spoilerToggle(spoilerID) {
 
 function singleQuestionAnswerSubmit(input_box, unit_box, answers, correct_display, spoiler, event='') {
     event.preventDefault();
-    var forms = document.querySelectorAll('form');
-    var formData = new FormData(document.querySelector('form'));
 
     var inputAns = parseInt(document.getElementById(input_box).value);
     var unitAns = document.getElementById(unit_box).value;
